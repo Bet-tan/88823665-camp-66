5d430b890f473c17d411df44bd3f0b14244d19f3
Bet-tan <ssbackup30@gmail.com>
2025-01-08 18:18:10 +0700

    route

--- myLaravel/routes/web.php
+++ myLaravel/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
+
+Route::get('/mycontroller/{id?}',
+    [MyController::class, 'myfunction']);
+
+Route::post('/mycontroller/{id?}',
+    [MyController::class, 'myfunction']);
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+route::get('/hello/{id?}',
+    function ($val="") {
+    return "<h1>Hello World $val</h1>";
 });
