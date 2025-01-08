fec918a93ec263ab37c1d76b434a06cb931fe10c
Bet-tan <ssbackup30@gmail.com>
2025-01-08 16:23:15 +0700

    Update php_01.php

---
+++ php_01.php
@@ -0,0 +1,15 @@
+<?php
+$h = 2;
+echo "สูตรคูณแม่ 2";
+echo "<br>";
+
+for($i = 1; $i < 13; $i++) {
+    echo $h;
+    echo " x ";
+    echo $i;
+    echo " = ";
+    echo $h*$i;
+    echo "<br>";
+
+}
+?>
