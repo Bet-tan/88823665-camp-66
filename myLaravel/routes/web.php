c73e64f75bd6374775e1b5e65e304bc751e6fafb
Bet-tan <ssbackup30@gmail.com>
2025-01-29 18:26:44 +0700

    e

--- myLaravel/routes/web.php
+++ myLaravel/routes/web.php
@@ -5,15 +5,22 @@ use App\Http\Controllers\MyController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::get('/register', [RegisterController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index']);
 Route::get('/', [HomeController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'create']);
 
 Route::get('/mycontroller/{id?}', [MyController::class, 'myfunction']);
 
 Route::post('/mycontroller/{id?}', [MyController::class, 'myfunction']);
+
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'edit']);
+Route::put
+Route::delete()
 
 Route::get('/', function () {
     return view('home');
